ace6f12571c6cd6e9a42fab4b37c35d44a1918c1
Emerson Laurentino <emerson@laurentino.co>
2022-03-30 14:15:20 -0300

    feat: resolver to search result

--- packages/api/src/platforms/vtex/resolvers/searchResult.ts
+++ packages/api/src/platforms/vtex/resolvers/searchResult.ts
@@ -1,41 +1,89 @@
-import { enhanceSku } from '../utils/enhanceSku'
 import type { Resolver } from '..'
 import type { SearchArgs } from '../clients/search'
 import type { Attribute } from '../clients/search/types/AttributeSearchResult'
+import type { ProductSearchResult } from '../clients/search/types/ProductSearchResult'
+import { enhanceSku } from '../utils/enhanceSku'
 
 type Root = Omit<SearchArgs, 'type'>
 
 const REMOVED_FACETS_FROM_COLLECTION_PAGE = ['departamento']
 
+function parseProducts(products: ProductSearchResult) {
+  const skus = products.products
+    .map((product) => {
+      const [maybeSku] = product.skus
+
+      return maybeSku && enhanceSku(maybeSku, product)
+    })
+    .filter((sku) => !!sku)
+
+  return {
+    pageInfo: {
+      hasNextPage: products.pagination.after.length > 0,
+      hasPreviousPage: products.pagination.before.length > 0,
+      startCursor: '0',
+      endCursor: products.total.toString(),
+      totalCount: products.total,
+    },
+    edges: skus.map((sku, index) => ({
+      node: sku,
+      cursor: index.toString(),
+    })),
+  }
+}
+
+function parseSuggestedProducts(products: ProductSearchResult) {
+  const skus = products.products
+    .map((product) => {
+      const [maybeSku] = product.skus
+
+      return maybeSku && enhanceSku(maybeSku, product)
+    })
+    .filter((sku) => !!sku)
+
+  return {
+    pageInfo: {
+      hasNextPage: false,
+      hasPreviousPage: false,
+      startCursor: '0',
+      endCursor: '5',
+      totalCount: 5,
+    },
+    edges: skus.map((sku, index) => ({
+      node: sku,
+      cursor: index.toString(),
+    })),
+  }
+}
+
 export const StoreSearchResult: Record<string, Resolver<Root>> = {
+  suggestions: async (searchArgs, _, ctx) => {
+    const {
+      clients: { search },
+    } = ctx
+
+    const suggestions = await search.suggestedTerms(searchArgs)
+
+    const {
+      suggestion: { searches },
+    } = suggestions
+
+    return searches
+  },
   products: async (searchArgs, _, ctx) => {
     const {
       clients: { search },
     } = ctx
 
+    if (searchArgs.suggestions) {
+      const products = await search.suggestedProducts(searchArgs)
+
+      return parseSuggestedProducts(products)
+    }
+
     const products = await search.products(searchArgs)
 
-    const skus = products.products
-      .map((product) => {
-        const [maybeSku] = product.skus
-
-        return maybeSku && enhanceSku(maybeSku, product)
-      })
-      .filter((sku) => !!sku)
-
-    return {
-      pageInfo: {
-        hasNextPage: products.pagination.after.length > 0,
-        hasPreviousPage: products.pagination.before.length > 0,
-        startCursor: '0',
-        endCursor: products.total.toString(),
-        totalCount: products.total,
-      },
-      edges: skus.map((sku, index) => ({
-        node: sku,
-        cursor: index.toString(),
-      })),
-    }
+    return parseProducts(products)
   },
   facets: async (searchArgs, _, ctx) => {
     const {
